b8c017871a351fa63ff289d1209b6af5d88a42dc
Justin Williams <justinw@me.com>
2026-02-19 10:12:14 -0700

    Update minimum deployment targets to iOS 15 / macOS 11

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "Zip",
+    platforms: [
+        .iOS(.v15),
+        .macCatalyst(.v15),
+        .macOS(.v11),
+        .tvOS(.v15),
+    ],
     products: [
         .library(name: "Zip", targets: ["Zip"])
     ],
